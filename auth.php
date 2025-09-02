002ec6952d80321cb2c9f0389e77cc1aea565091
Jose A. García <joaghix@users.noreply.github.com>
2025-09-02 11:08:43 +0200

    Update auth.php
    Si existe la variable WantsUrl, pasarla al SSO para que se mantenga la redirección al recurso concreto solicitado por el usuario.

--- auth.php
+++ auth.php
@@ -96,8 +96,15 @@ class auth_plugin_userkey extends auth_plugin_base {
      * It redirects a user if required or return true.
      */
     public function loginpage_hook() {
+        global $SESSION;
+        
         if ($this->should_login_redirect()) {
-            $this->redirect($this->config->ssourl);
+            if (empty($SESSION->wantsurl)) {
+                $this->redirect($this->config->ssourl);
+            }
+            else {
+                $this->redirect($this->config->ssourl . urlencode("?wantsurl=" . $SESSION->wantsurl));
+            }
         }
 
         return true;
